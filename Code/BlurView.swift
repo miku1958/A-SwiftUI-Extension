2bdd4cf28e2df0f3ab924f152dc4c7e582b34072
mikun <1211153686@qq.com>
2019-09-19 15:27:06 +0800

    update BlurView

--- Code/BlurView.swift
+++ Code/BlurView.swift
@@ -7,86 +7,112 @@
 //
 
 import SwiftUI
-#if os(iOS)
-struct BlurView: UIViewRepresentable {
+
+@available(iOS 13.0, OSX 10.15, *)
+@available(tvOS, unavailable)
+@available(watchOS, unavailable)
+struct BlurView {
 	/*0 to 1*/
-    let level: CGFloat
+    fileprivate let level: CGFloat
+	fileprivate let _level: Any?
+	
 	enum Sort {
 		case light
 		case dark
 	}
-	let sort: Sort
-	init(level: CGFloat, prefer sort: Sort = .light) {
+	fileprivate let sort: Sort
+	
+	fileprivate var configs: [(ViewType)->()] = []
+	
+	public init(level: CGFloat, prefer sort: Sort = .light) {
+		self._level = nil
 		self.level = level
 		self.sort = sort
 	}
-
+}
+#if os(iOS)
+extension BlurView: UIViewRepresentable {
+	typealias ViewType = UIVisualEffectView
     func makeUIView(context: UIViewRepresentableContext<BlurView>) -> UIVisualEffectView {
-		let blurEffect = UIBlurEffect(style: style)
-        let blurView = UIVisualEffectView(effect: blurEffect)
+        let blurView = UIVisualEffectView()
         blurView.translatesAutoresizingMaskIntoConstraints = false
-
         return blurView
     }
 
     func updateUIView(_ uiView: UIVisualEffectView, context: UIViewRepresentableContext<BlurView>) {
-		uiView.effect = UIBlurEffect(style: style)
+		uiView.effect = UIBlurEffect(style: self._level as? UIBlurEffect.Style ?? self.style)
     }
-}
-extension BlurView {
-	fileprivate var style: UIBlurEffect.Style {
-		var styles: [UIBlurEffect.Style]
+	fileprivate static func _styles(sort: Sort) -> [UIBlurEffect.Style] {
 		switch sort {
 		case .light:
-			styles = [.regular, .systemUltraThinMaterial, .systemThinMaterial, .prominent, .systemMaterial, .systemChromeMaterial, .systemThickMaterial]
+			return [.regular, .systemUltraThinMaterial, .systemThinMaterial, .prominent, .systemMaterial, .systemChromeMaterial, .systemThickMaterial]
 		case .dark:
-			styles = [.systemUltraThinMaterial, .systemChromeMaterial, .systemThinMaterial, .regular, .systemMaterial, .prominent, .systemThickMaterial]
+			return [.systemUltraThinMaterial, .systemChromeMaterial, .systemThinMaterial, .regular, .systemMaterial, .prominent, .systemThickMaterial]
 		}
-		
+	}
+	fileprivate var style: UIBlurEffect.Style {
+		let styles = Self._styles(sort: sort)
 		let index = Int(round(CGFloat(styles.count)) * level)
-		print(index)
 		return styles[max(0, min(styles.count - 1, index))]
 	}
 }
+@available(iOS 13.0, *)
+@available(OSX, unavailable)
+@available(tvOS, unavailable)
+@available(watchOS, unavailable)
+extension BlurView {
+	public init(style: UIBlurEffect.Style) {
+		self._level = style
+		self.level = 0
+		self.sort = .light
+	}
+}
 #elseif os(macOS)
-@available(OSX 10.15, *)
-struct BlurView: NSViewRepresentable {
-	/*0 to 1*/
-    let level: CGFloat
-	enum Sort {
-		case light
-		case dark
-	}
-	let sort: Sort
-	init(level: CGFloat, prefer sort: Sort = .light) {
-		self.level = level
-		self.sort = sort
-	}
-
+extension BlurView: NSViewRepresentable {
+	
+	typealias ViewType = NSVisualEffectView
 	func makeNSView(context: NSViewRepresentableContext<BlurView>) -> NSVisualEffectView {
 		
         let blurView = NSVisualEffectView()
         blurView.translatesAutoresizingMaskIntoConstraints = false
-		blurView.material = style
-		blurView.blendingMode = .withinWindow
         return blurView
     }
 
 	func updateNSView(_ nsView: NSVisualEffectView, context: NSViewRepresentableContext<BlurView>) {
-		nsView.material = style
+		nsView.material = _level as? NSVisualEffectView.Material ?? style
+		configs.forEach { $0(nsView) }
 	}
-}
-extension BlurView {
-	fileprivate var style: NSVisualEffectView.Material {
-		var styles: [NSVisualEffectView.Material]
+	fileprivate static func _styles(sort: Sort) -> [NSVisualEffectView.Material] {
 		switch sort {
-		case .light:
-			styles = [.popover, .toolTip, .menu, .sheet, .headerView, .sidebar, .underWindowBackground, .windowBackground, .contentBackground]
-		case .dark:
-			styles = [.popover, .toolTip, .menu, .sheet, .headerView, .sidebar, .underWindowBackground, .windowBackground, .contentBackground]
-		}
+			case .light:
+				return [.popover, .toolTip, .menu, .sheet, .headerView, .sidebar, .underWindowBackground, .windowBackground, .contentBackground]
+			case .dark:
+				return [.popover, .toolTip, .menu, .sheet, .headerView, .sidebar, .underWindowBackground, .windowBackground, .contentBackground]
+			}
+	}
+	fileprivate var style: NSVisualEffectView.Material {
+		let styles = Self._styles(sort: sort)
+		
 		let index = Int(round(CGFloat(styles.count)) * level)
 		return styles[max(0, min(styles.count - 1, index))]
+	}
+}
+@available(OSX 10.15, *)
+@available(iOS, unavailable)
+@available(tvOS, unavailable)
+@available(watchOS, unavailable)
+extension BlurView {
+	public init(style: NSVisualEffectView.Material) {
+		self._level = style
+		self.level = 0
+		self.sort = .light
+	}
+	public func blendingMode(_ mode: NSVisualEffectView.BlendingMode) -> Self {
+		var view = self
+		view.configs.append {
+			$0.blendingMode = mode
+		}
+		return view
 	}
 }
 #endif
